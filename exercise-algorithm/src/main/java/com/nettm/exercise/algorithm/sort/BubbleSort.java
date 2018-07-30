fc0baeb3c338ffc0519d3063d53bb326944bc940
nettm <nettm@163.com>
2018-07-30 20:27:50 +0800

    finished bubble sort

--- exercise-algorithm/src/main/java/com/nettm/exercise/algorithm/sort/BubbleSort.java
+++ exercise-algorithm/src/main/java/com/nettm/exercise/algorithm/sort/BubbleSort.java
@@ -2,8 +2,18 @@ package com.nettm.exercise.algorithm.sort;
 
 public class BubbleSort implements Sort {
 
-	@Override 
+	@Override
 	public int[] sort(int[] args) {
+		for (int i = args.length - 1; i >= 0; i--) {
+			for (int k = (i - 1); k >= 0; k--) {
+				if (args[i] < args[k]) {
+					int a = args[k];
+					args[k] = args[i];
+					args[i] = a;
+				}
+			}
+		}
+
 		return args;
 	}
 
